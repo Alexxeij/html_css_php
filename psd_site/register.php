15cbc65a03a588b913a8cc1f60c81a3ae43a0dee
Alexxeij <kolesnik.alexei1@gmail.com>
2016-12-07 13:36:26 +0400

    method_GET

--- psd_site/register.php
+++ psd_site/register.php
@@ -1,9 +1,10 @@
 <?php
 
 
-  if ($_POST) {
-    $string = serialize($_POST);
+ if ($_GET) {
+    $string = serialize($_GET);
     $f = fopen('users.txt', 'a');
-    fwrite($f, $string . PHP_EOL);
+    fwrite($f, $string.PHP_EOL);
     fclose($f);
+     header("Location:reg_user_thanks.html");
 }
